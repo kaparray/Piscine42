b6e4dc8bf45e27e0ddb8161e493487157237c248
kaparray <kaparray@gmail.com>
2018-11-03 01:37:51 +0300

    Finish day02 and day03

---
+++ day03/ex09/ft_sort_integer_table.c
@@ -0,0 +1,35 @@
+ /* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_integer_table.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hpowlows <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/21 21:24:45 by hpowlows          #+#    #+#             */
+/*   Updated: 2018/09/21 22:13:38 by hpowlows         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	sort_integer_table(int *tab, int size)
+{
+	int i;
+	int j;
+	int temp;
+
+	i = 0;
+	while (i < size)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (tab[i] > tab[j])
+			{
+				temp = tab[i];
+				tab[i] = tab[j];
+				tab[j] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
